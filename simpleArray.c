f51c26a7e9de478e8dbf843cf965439e26b91a6a
sdeppe <Shawn.deppe42@gmail.com>
2024-01-09 12:17:54 +0900

    week 5

---
+++ simpleArray.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	const int SIZE = 5;
+
+	int grades[SIZE] = {78, 67, 92, 83, 88};
+	double sum = 0.0;
+	int i;
+
+	printf("\n my grades are: \n");
+
+	for (i = 0; i < SIZE; i++)
+		printf("%d\t", grades[i]);
+
+	printf("\n\n");
+
+	for(i = 0; i < SIZE; i++)
+		sum = sum + grades[i];
+
+	printf("my average is %.2f\n\n", sum/SIZE);
+
+	return 0;
+}
